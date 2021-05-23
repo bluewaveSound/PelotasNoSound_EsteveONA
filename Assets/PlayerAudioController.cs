99919ca7bbfe3cbf481094fd25365a099b88b417
bluewaveSound <83900944+bluewaveSound@users.noreply.github.com>
2021-05-23 20:19:52 +0200

    Add files via upload

---
+++ Assets/PlayerAudioController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioController : MonoBehaviour
+{
+    // This is an ARRAY !
+    AudioSource[] sources;
+    Rigidbody rb;
+    float speed = 0;
+    float weight = 0.0f;
+    bool isPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sources = GetComponents<AudioSource>();  
+	    rb = GetComponent<Rigidbody>();
+        weight = rb.mass;
+        sources[1] .pitch = 1.0f / rb.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    speed = rb.velocity.magnitude;
+
+	    // If speed is greater than 0.1 AND (&&) We are NOT [1] playing the sound
+	    if(speed > 0.1 && isPlaying) {
+	       isPlaying = true;
+	       sources [0].Play();
+	    } else if (speed < 0.1 && isPlaying) {
+           isPlaying = false;
+	       sources[0].Stop();
+        }
+
+        sources[0].pitch = speed / (weight * 2.0f);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("player collision" + collision.collider.name);
+        sources[1].Play(); 
+    }
+
+}
+
